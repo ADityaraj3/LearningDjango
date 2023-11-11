d807b513e254d3b5ea5d4a456995eaf8f6f599cf
ADityaraj3 <77296151+ADityaraj3@users.noreply.github.com>
2023-11-11 18:57:09 +0000

    MoveToLaptop

--- app.py
+++ app.py
@@ -1,6 +1,47 @@
 from flask import Flask, render_template, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Postgres1234@localhost:5432/jobsdata'
+
+db=SQLAlchemy(app)
+
+class jobsTable(db.Model):
+  __tablename__='dataOFJobs'
+  id=db.Column(db.Integer,primary_key=True)
+  companyName=db.Column(db.String(40))
+  title=db.Column(db.String(40))
+  location=db.Column(db.String(40))
+  salary=db.Column(db.String(40))
+
+  def __init__(self,companyName,title,location,salary):
+    self.companyName=companyName
+    self.title=title
+    self.location=location
+    self.salary=salary
+
+app.app_context().push()
+
+
+# with app.app_context():
+#   db.create_all()
+
+# app.route('/submit', methods=['POST'])
+# def submit():
+#   fname= request.form['fname']
+#   lname=request.form['lname']
+#   pet=request.form['pets']
+
+#   student=Student(fname,lname,pet)
+#   db.session.add(student)
+#   db.session.commit()
+
+#   #fetch a certain student2
+#   studentResult=db.session.query(Student).filter(Student.id==1)
+#   for result in studentResult:
+#     print(result.fname)
+
+#   return render_template('success.html', data=fname)
 
 JOBS = [
   {
